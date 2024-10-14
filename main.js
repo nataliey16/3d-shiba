1cbba223ac9d9204d647511d5c4e387c1660fd2f
Natalie <natalie.yeung16@gmail.com>
2024-10-14 14:54:43 -0600

    Adding jumping functionality to dog model

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import gsap from "gsap";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -25,7 +26,7 @@ const loader = new GLTFLoader();
 
 let shiba;
 loader.load(
-  "./public/shiba.glb",
+  "./shiba.glb",
 
   function (gltf) {
     shiba = gltf.scene;
@@ -41,7 +42,39 @@ camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
+  controls.update(); // Update controls with damping
   renderer.render(scene, camera);
 }
 
+//coordinations of dog and where user clicks on screen
+//Raycasting used to determine what objects in the 3d space the mouse is over
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+// handle mouse-down events to detect clicks on dog to trigger a jump
+function onDocumentMouseDown(event) {
+  event.preventDefault();
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(scene.children, true);
+
+  if (intersects.length > 0) {
+    const jumpHeight = 1.5;
+    const jumpDuration = 0.4;
+    gsap.to(shiba.position, {
+      y: jumpHeight,
+      duration: jumpDuration,
+      yoyo: true,
+      repeat: 1,
+      ease: "power1.inOut",
+    });
+  }
+}
+
+document.addEventListener("mousedown", onDocumentMouseDown, false);
+//recursively is called
 animate();
